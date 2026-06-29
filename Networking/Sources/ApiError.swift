354ccf77d53fdb915b497f1c71a061c9a5fdcc98
Pawel Kacela <pawel.kacela@gmail.com>
2026-06-29 18:51:22 +0200

    add, lozalized descriptions

--- Networking/Sources/ApiError.swift
+++ Networking/Sources/ApiError.swift
@@ -5,12 +5,26 @@
 //  Created by Pawel Kacela on 29/06/2026.
 //
 
+import Foundation
 
-enum ApiError: Error {
+public enum ApiError: Error, LocalizedError {
     case invalidResponse
     case requestFailed(statusCode: Int, message: String?)
     case networkError(Error)
     case taskCancellation
+    
+    public var errorDescription: String? {
+        switch self {
+        case .invalidResponse:
+            "Invalid response"
+        case .requestFailed(statusCode: let statusCode, message: let message):
+            "Request failed with status code: \(statusCode). \(message ?? "")"
+        case .networkError(let error):
+            error.localizedDescription
+        case .taskCancellation:
+            "Request was cancelled"
+        }
+    }
 }
 
 struct ServerError: Decodable {
