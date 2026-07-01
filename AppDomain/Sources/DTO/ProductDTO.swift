f77c8c8e3b2e9ed2b5549165ef0ff334270a7cfb
Pawel Kacela <pawel.kacela@gmail.com>
2026-07-01 19:51:27 +0200

    add: initial files, basicly placeholders

--- AppDomain/Sources/DTO/ProductDTO.swift
+++ AppDomain/Sources/DTO/ProductDTO.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import Networking
+
+public struct ProductDTO: Identifiable {
+    public let id: Int
+    public let title: String
+    public let price: Double
+}
+
+extension ProductDTO: Sendable {
+    static let example = ProductDTO(id: 1, title: "Example", price: 10.0)
+}
--- AppDomain/Sources/Services/ProductService.swift
+++ AppDomain/Sources/Services/ProductService.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import Networking
+
+protocol ProductService {
+    func fetch(skip: Int, limit: Int) async throws -> [Product]
+}
+
+struct DefaultProductService: ProductService {
+    
+    let client = APIClient()
+    let baseURL: URL
+    
+    init(baseURL: URL = URL(string: "https://dummyjson.com")!
+    ) {
+        self.baseURL = baseURL
+    }
+    
+    func fetch(skip: Int, limit: Int) async throws -> [Product] {
+        let endpoint = ProductEndpoint(limit: limit, skip: skip)
+        let request = try endpoint.makeRequest(baseURL: baseURL)
+        
+        return try await client.fetch(request: request,
+                                      ProductsResponse.self).products
+    }
+
+}
+
+struct MockProductService: ProductService {
+    
+    let error: ApiError?
+    let result: [Product]
+    
+    init(error: ApiError? = nil,
+         result: [Product] = [Product.example]
+    ) {
+        self.error = error
+        self.result = result
+    }
+    
+    func fetch(skip: Int, limit: Int) async throws -> [Product] {
+        if let error {
+            throw error
+        } else {
+            return result
+        }
+    }
+}
+
+
