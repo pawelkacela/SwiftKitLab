26cdd67b14de19e90b3ae180be1b730fc7e191c5
Pawel Kacela <pawel.kacela@gmail.com>
2026-07-02 17:51:53 +0200

    add: models for UI

--- AppDomain/Sources/DTO/ProductDTO.swift
+++ AppDomain/Sources/DTO/ProductDTO.swift
@@ -6,14 +6,22 @@
 //
 
 import Foundation
-import Networking
 
 public struct ProductDTO: Identifiable {
     public let id: Int
     public let title: String
     public let price: Double
+    
+    public init(id: Int,
+                title: String,
+                price: Double
+    ) {
+        self.id = id
+        self.title = title
+        self.price = price
+    }
 }
 
 extension ProductDTO: Sendable {
-    static let example = ProductDTO(id: 1, title: "Example", price: 10.0)
+    public static let example = ProductDTO(id: 1, title: "Example", price: 10.0)
 }
