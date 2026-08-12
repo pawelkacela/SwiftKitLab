6d43674e26f2d1c1e1a3fc32b3cf164d87c9455c
Pawel Kacela <pawel.kacela@gmail.com>
2026-08-12 14:00:42 +0200

    add: headers test

--- Networking/Tests/EndpointTests.swift
+++ Networking/Tests/EndpointTests.swift
@@ -18,10 +18,31 @@ struct EndpointTests {
         let components = try #require(URLComponents(url: requestURL, resolvingAgainstBaseURL: false))
         #expect(components.queryItems == [URLQueryItem(name: "limit", value: "10")])
     }
+    
+    @Test func includesEndpointHeadersInRequest() throws {
+        let endpoint = HeaderEndpoint()
+        let baseURL = try #require(URL(string: "https://example.com"))
+        
+        let request = try endpoint.makeRequest(baseURL: baseURL)
+        
+        #expect(request.value(forHTTPHeaderField: "Content-Type") == "application/json")
+        #expect(request.value(forHTTPHeaderField: "Accept") == "application/json")
+    }
 }
 
 private struct TestEndpoint: Endpoint {
     let path = "products"
     let method = HTTPMethod.get
     let queryItems = [URLQueryItem(name: "limit", value: "10")]
+}
+
+private struct HeaderEndpoint: Endpoint {
+    let path = "login"
+    let method = HTTPMethod.post
+    let queryItems: [URLQueryItem] = []
+
+    let headers = [
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    ]
 }
