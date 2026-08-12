4891f7451c74a8fd9036a85f428d7e65ce57549e
Pawel Kacela <pawel.kacela@gmail.com>
2026-08-12 15:50:33 +0200

    refactor: reorder project folders

---
+++ AppDomain/Sources/Products/Services/ProductService.swift
@@ -0,0 +1,12 @@
+//
+//  ProductService.swift
+//  AppDomain
+//
+//  Created by Pawel Kacela on 01/07/2026.
+//
+
+import Foundation
+
+public protocol ProductService {
+    func fetch(skip: Int, limit: Int) async throws -> [Product]
+}
